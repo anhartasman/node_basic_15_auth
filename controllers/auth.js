898e5d8236074fe0b546fc1eab81bc2cb740886a
Anhar <anhar_tasman@yahoo.com>
2025-04-22 12:04:47 +0700

    login flash message

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,23 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length>0){
+    message = message[0];
+  }else{
+    message = null
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false
+    errorMessage:message
   });
 };
 
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
-    pageTitle: 'Signup',
-    isAuthenticated: false
+    pageTitle: 'Signup'
   });
 };
 
@@ -23,6 +28,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email:email})
     .then(user => {
       if(!user){
+        req.flash('error','Invalid email.'); 
         return res.redirect('/login');
       }
       bcrypt.compare(password,user.password).then(doMatch=>{
@@ -34,6 +40,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
           }); 
         }
+        req.flash('error','Invalid password.');
         res.redirect('/login');
       }).catch(err=>{
         console.log(err);
