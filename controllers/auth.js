9c3b6d7ed0d2b2f2d850cc0d5db13fe28aebae70
Anhar <anhar_tasman@yahoo.com>
2025-04-29 13:26:25 +0700

    validation setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 
 const transporter = nodemailer.createTransport({
   host: 'in-v3.mailjet.com',
@@ -72,6 +73,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage:errors.array()
+    });
+  }
   User.findOne({email:email}).then(userDoc=>{
     if(userDoc){
       req.flash('error','E-mail exists already, please pick a different one'); 
