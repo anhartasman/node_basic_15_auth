ce59ba16b02f789f0b302e3d565ce9f9d1e69cc6
Anhar <anhar_tasman@yahoo.com>
2025-08-12 11:52:34 +0700

    keeping user input

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,7 +37,12 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage:message
+    errorMessage:message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
   });
 };
 
@@ -78,7 +83,8 @@ exports.postSignup = (req, res, next) => {
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage:errors.array()[0].msg
+      errorMessage:errors.array()[0].msg,
+      oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword, },
     });
   }
     return bcrypt.hash(password,12).then(hashedPassword=>{
