8e2e6697b2bd2c443893f7eae9ab909e4e4c04db
Anhar <anhar_tasman@yahoo.com>
2025-08-23 23:56:15 +0700

    sanitizing Data

--- routes/auth.js
+++ routes/auth.js
@@ -11,10 +11,12 @@ router.get('/login', authController.getLogin);
 router.get('/signup', authController.getSignup);
 
 router.post('/login',[
-    body('email').isEmail().withMessage('Please enter a valid email address'),
+    body('email').isEmail().withMessage('Please enter a valid email address')
+    .normalizeEmail(),
     body('password','Password has to be valid.').isLength({
         min:5
     }).isAlphanumeric()
+    .trim()
 ], authController.postLogin);
 
 router.post('/signup', [
@@ -28,9 +30,10 @@ router.post('/signup', [
           return Promise.reject('E-mail exists already, please pick a different one');
         }
     });
-}),
-body('password','Please enter a password with only numbers and text and at least 5 characters.').isLength({min:5}).isAlphanumeric(),
-body('confirmPassword').custom((value, { req }) => {
+}).normalizeEmail(),
+body('password','Please enter a password with only numbers and text and at least 5 characters.').isLength({min:5}).isAlphanumeric()
+.trim(),
+body('confirmPassword').trim().custom((value, { req }) => {
     if (value !== req.body.password){
         throw new Error("Passwords have to match!");
     }
