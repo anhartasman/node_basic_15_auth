1df29da2bf24737f54338464fa4e9081f773dfc1
Anhar <anhar_tasman@yahoo.com>
2025-12-25 12:24:47 +0700

    filtering files by mimetype

--- app.js
+++ app.js
@@ -42,6 +42,17 @@ const fileStorage = multer.diskStorage({
     cb(null, new Date().toISOString() + "-" + file.originalname);
   },
 });
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 /* ===============================
    View engine
    =============================== */
@@ -59,7 +70,9 @@ const authRoutes = require("./routes/auth");
    Middleware
    =============================== */
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image"));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
